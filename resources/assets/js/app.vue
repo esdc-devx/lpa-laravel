15de249c1604e872d1d54f044336248cbea72f87
Keven Rousseau <keven.rousseau@canada.ca>
2018-03-09 15:40:34 -0500

    PR16: make component for content

--- resources/assets/js/app.vue
+++ resources/assets/js/app.vue
@@ -16,11 +16,7 @@
         <el-container class="content-wrap">
           <el-main>
             <breadcrumb/>
-            <keep-alive>
-              <transition :name="transitionName">
-                <router-view/>
-              </transition>
-            </keep-alive>
+            <main-content/>
           </el-main>
         </el-container>
       </el-container>
@@ -32,71 +28,21 @@
 </template>
 
 <script>
-  import { mapGetters } from 'vuex';
   import EventBus from './helpers/event-bus';
 
   import TopBar from './views/top-bar.vue';
   import SideBar from './views/side-bar.vue';
   import Breadcrumb from './views/breadcrumb.vue';
+  import MainContent from './views/main-content.vue';
 
   export default {
     name: 'app',
 
-    components: { TopBar, SideBar, Breadcrumb },
-
-    computed: {
-      ...mapGetters([
-        'language'
-      ])
-    },
+    components: { TopBar, SideBar, Breadcrumb, MainContent },
 
     data() {
       return {
-        isLoading: true,
-        transitionName: 'fade'
-      }
-    },
-
-    watch: {
-      // handles the transition when changing pages
-      $route(to, from) {
-        // example url transitions:
-        //    /en/projects => /en/projects/1:       swipe
-        //    /en/projects/1 => /en/projects:       swipe
-        //    /en/projects/1 => /en/admin/users:    fade
-        //    /en/admin/users => /en:               fade
-
-        const toDepth = to.path.split('/').length;
-        const fromDepth = from.path.split('/').length;
-        let isSameLevel = toDepth === fromDepth;
-        let isFromOrToHome = from.path === '/' || from.path === `/${this.language}` || to.path === `/${this.language}`;
-
-        // check for direct hierarchy
-        let isParentChild = this.isParentChild(to, from);
-        // when same level, => fade
-        // when parent-child, => swipe
-        if (isSameLevel || !isParentChild || isFromOrToHome) {
-          this.transitionName = 'fade';
-        } else {
-          this.transitionName = toDepth < fromDepth ? 'swipe-right' : 'swipe-left';
-        }
-      }
-    },
-
-    methods: {
-      isParentChild(to, from) {
-        const toSplit = to.path.split('/');
-        const fromSplit = from.path.split('/');
-        const toDepth = toSplit.length;
-        const fromDepth = fromSplit.length;
-
-        // look up max 2 level up in case we get url change like so:
-        // /en/admin/users => /en/admin/users/edit/3
-        if (fromSplit[fromDepth - 1] === toSplit[toDepth - 2] || toSplit[toDepth - 1] === fromSplit[fromDepth - 2] ||
-            fromSplit[fromDepth - 1] === toSplit[toDepth - 3] || toSplit[toDepth - 1] === fromSplit[fromDepth - 3]) {
-          return true;
-        }
-        return false;
+        isLoading: true
       }
     },
 
---
+++ resources/assets/js/views/main-content.vue
@@ -0,0 +1,78 @@
+<template>
+  <keep-alive>
+    <transition :name="transitionName">
+      <router-view/>
+    </transition>
+  </keep-alive>
+</template>
+
+<script>
+  import { mapGetters } from 'vuex';
+
+  export default {
+    name: 'main-content',
+
+    data() {
+      return {
+        transitionName: 'fade'
+      }
+    },
+
+    computed: {
+      ...mapGetters([
+        'language'
+      ])
+    },
+
+    watch: {
+      // handles the transition when changing pages
+      $route(to, from) {
+        // example url transitions:
+        //    /en/projects => /en/projects/1:       swipe
+        //    /en/projects/1 => /en/projects:       swipe
+        //    /en/projects/1 => /en/admin/users:    fade
+        //    /en/admin/users => /en:               fade
+
+        const toDepth = to.path.split('/').length;
+        const fromDepth = from.path.split('/').length;
+        let isSameLevel = toDepth === fromDepth;
+        let isFromOrToHome = from.path === '/' || from.path === `/${this.language}` || to.path === `/${this.language}`;
+
+        // check for direct hierarchy
+        let isParentChild = this.isParentChild(to, from);
+        // when same level, => fade
+        // when parent-child, => swipe
+        if (isSameLevel || !isParentChild || isFromOrToHome) {
+          this.transitionName = 'fade';
+        } else {
+          this.transitionName = toDepth < fromDepth ? 'swipe-right' : 'swipe-left';
+        }
+      }
+    },
+
+    methods: {
+      isParentChild(to, from) {
+        const toSplit = to.path.split('/');
+        const fromSplit = from.path.split('/');
+        const toDepth = toSplit.length;
+        const fromDepth = fromSplit.length;
+
+        // this basically checks for when we are going back and forward
+        // for backwards actions: if the FROM last slug is equal to the TO last - 1 slug
+        // for forwards actions:  if the TO last slug is equal to the FROM last - 1 slug
+        // then this l;ogic is duplicated
+        // for max 2 level up in case we get url change like so:
+        // /en/admin/users => /en/admin/users/edit/3
+        if (fromSplit[fromDepth - 1] === toSplit[toDepth - 2] || toSplit[toDepth - 1] === fromSplit[fromDepth - 2] ||
+            fromSplit[fromDepth - 1] === toSplit[toDepth - 3] || toSplit[toDepth - 1] === fromSplit[fromDepth - 3]) {
+          return true;
+        }
+        return false;
+      }
+    }
+  };
+</script>
+
+<style lang="scss">
+
+</style>
