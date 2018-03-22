37bebc16e08fa91012ae9ef812d57ce8682b435b
Keven Rousseau <keven.rousseau@canada.ca>
2018-03-22 09:35:25 -0400

    PR-23: add comment in config for throttle_wait_time

--- resources/assets/js/config.js
+++ resources/assets/js/config.js
@@ -1,6 +1,6 @@
 export default Object.freeze({
   DEBUG: process.env.NODE_ENV !== 'production',
   DEFAULT_LANG: document.querySelector('html').lang,
-
+  // this is to ensure that actions are throttled to avoid user click spam
   THROTTLE_WAIT_TIME: 500
 });
