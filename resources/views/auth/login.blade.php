a46f9b1691b204746f76509e295b5aa114fab97e
Keven Rousseau <keven.rousseau@canada.ca>
2018-03-28 13:43:33 -0400

    PR-24: move login styling into head

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,59 +12,6 @@
 
     <!-- Styles -->
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
-</head>
-<body>
-    <div id="app" v-cloak>
-        <el-container>
-            <el-header height="50px">
-                <div class="top-bar">
-                    <h1>{{ __('navigation.app_name') }}</h1>
-                    <a v-if="toggledLocale === 'fr'" :href="'/' + toggledLocale + '/login'" class="lang">Français</a>
-                    <a v-else :href="'/' + toggledLocale + '/login'" class="lang">English</a>
-                </div>
-            </el-header>
-            <el-main>
-                <div class="login-wrap">
-                    <div><h2>{{ __('login.title') }}</h2></div>
-                    <el-form
-                        ref="form"
-                        label-width="30%"
-                        method="POST"
-                        action="{{ route('login') }}">
-                        {{ csrf_field() }}
-                        <el-form-item label="{{ __('login.username') }}" for="username" :class="['is-required', {'is-error': verrors.collect('username').length }]" prop="username">
-                            <el-input id="username" name="username" v-model="username" v-validate="'required'" @keyup.native.enter="onSubmit" autofocus></el-input>
-                            <form-error v-for="error in verrors.collect('username')" :key="error.id">@{{ error }}</form-error>
-                        </el-form-item>
-                        <el-form-item label="{{ __('login.password') }}" for="password" :class="['is-required', {'is-error': verrors.collect('password').length }]" prop="password">
-                            <el-input id="password" name="password" :type="isPasswordVisible ? 'text' : 'password'" v-model="password" v-validate="'required'" @keyup.native.enter="onSubmit">
-                                <i
-                                    class="el-icon-view el-input__icon"
-                                    slot="suffix"
-                                    @mousedown="isPasswordVisible = true"
-                                    @mouseup="isPasswordVisible = false">
-                                </i>
-                            </el-input>
-                            <form-error v-for="error in verrors.collect('password')" :key="error.id">@{{ error }}</form-error>
-                        </el-form-item>
-                        <el-form-item label="{{ __('login.remember') }}" for="remember">
-                            <el-switch name="remember" v-model="remember"></el-switch>
-                        </el-form-item>
-
-                        <el-form-item class="controls-wrap">
-                            <el-button type="primary" :loading="isSaving" @click="onSubmit">{{ __('login.login') }}</el-button>
-                        </el-form-item>
-                    </el-form>
-                </div>
-            </el-main>
-        </el-container>
-    </div>
-    <div class="loading-spinner">
-        <svg viewBox="25 25 50 50" class="circular"><circle cx="50" cy="50" r="20" fill="none" class="path"></circle></svg>
-    </div>
-
-    <!-- Scripts -->
-    <script src="{{ mix('js/login.js') }}"></script>
     <style>
         body {
             font-family: "Segoe UI", "Arial", sans-serif;
@@ -140,5 +87,58 @@
             color: #524f74;
         }
     </style>
+</head>
+<body>
+    <div id="app" v-cloak>
+        <el-container>
+            <el-header height="50px">
+                <div class="top-bar">
+                    <h1>{{ __('navigation.app_name') }}</h1>
+                    <a v-if="toggledLocale === 'fr'" :href="'/' + toggledLocale + '/login'" class="lang">Français</a>
+                    <a v-else :href="'/' + toggledLocale + '/login'" class="lang">English</a>
+                </div>
+            </el-header>
+            <el-main>
+                <div class="login-wrap">
+                    <div><h2>{{ __('login.title') }}</h2></div>
+                    <el-form
+                        ref="form"
+                        label-width="30%"
+                        method="POST"
+                        action="{{ route('login') }}">
+                        {{ csrf_field() }}
+                        <el-form-item label="{{ __('login.username') }}" for="username" :class="['is-required', {'is-error': verrors.collect('username').length }]" prop="username">
+                            <el-input id="username" name="username" v-model="username" v-validate="'required'" @keyup.native.enter="onSubmit" autofocus></el-input>
+                            <form-error v-for="error in verrors.collect('username')" :key="error.id">@{{ error }}</form-error>
+                        </el-form-item>
+                        <el-form-item label="{{ __('login.password') }}" for="password" :class="['is-required', {'is-error': verrors.collect('password').length }]" prop="password">
+                            <el-input id="password" name="password" :type="isPasswordVisible ? 'text' : 'password'" v-model="password" v-validate="'required'" @keyup.native.enter="onSubmit">
+                                <i
+                                    class="el-icon-view el-input__icon"
+                                    slot="suffix"
+                                    @mousedown="isPasswordVisible = true"
+                                    @mouseup="isPasswordVisible = false">
+                                </i>
+                            </el-input>
+                            <form-error v-for="error in verrors.collect('password')" :key="error.id">@{{ error }}</form-error>
+                        </el-form-item>
+                        <el-form-item label="{{ __('login.remember') }}" for="remember">
+                            <el-switch name="remember" v-model="remember"></el-switch>
+                        </el-form-item>
+
+                        <el-form-item class="controls-wrap">
+                            <el-button type="primary" :loading="isSaving" @click="onSubmit">{{ __('login.login') }}</el-button>
+                        </el-form-item>
+                    </el-form>
+                </div>
+            </el-main>
+        </el-container>
+    </div>
+    <div class="loading-spinner">
+        <svg viewBox="25 25 50 50" class="circular"><circle cx="50" cy="50" r="20" fill="none" class="path"></circle></svg>
+    </div>
+
+    <!-- Scripts -->
+    <script src="{{ mix('js/login.js') }}"></script>
 </body>
 </html>
