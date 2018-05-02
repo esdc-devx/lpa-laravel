1d565227e2e62d48bf2ba580f728c088b8e3bdae
Keven Rousseau <keven.rousseau@canada.ca>
2018-05-02 14:47:41 -0400

    Fix HMR not compiling and reloading, Remove browserSync listener, Update packages

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,10 +14,24 @@ let mix = require('laravel-mix');
 mix.js('resources/assets/js/login.js', 'public/js')
    .js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css')
-   .sourceMaps()
-   .browserSync({
-     proxy: "localhost:8000",
-     port: "8000",
-     reloadDelay: 0,
-     notify: false
-   });
+   .sourceMaps();
+
+// This fixes HMR not compiling the assets and reloading the browser
+// with laravel-mix@^2.0
+// https://github.com/JeffreyWay/laravel-mix/issues/1483#issuecomment-366685986
+Mix.listen('configReady', (webpackConfig) => {
+  if (Mix.isUsing('hmr')) {
+    // Remove leading '/' from entry keys
+    webpackConfig.entry = Object.keys(webpackConfig.entry).reduce((entries, entry) => {
+      entries[entry.replace(/^\//, '')] = webpackConfig.entry[entry];
+      return entries;
+    }, {});
+
+    // Remove leading '/' from ExtractTextPlugin instances
+    webpackConfig.plugins.forEach((plugin) => {
+      if (plugin.constructor.name === 'ExtractTextPlugin') {
+        plugin.filename = plugin.filename.replace(/^\//, '');
+      }
+    });
+  }
+});
