f8e67c7c1fa69bbe452d1bffb6abeece55bea4b1
Keven Rousseau <keven.rousseau@canada.ca>
2018-05-09 13:38:52 -0400

    Add reference and docs

--- resources/assets/js/mixins/table/utils.js
+++ resources/assets/js/mixins/table/utils.js
@@ -13,12 +13,25 @@ export default {
     },
 
     onSort(a, b) {
-      // sort strings
-      if (_.isString(a) && _.isString(b)) {
-        return a.toLowerCase().localeCompare(b.toLowerCase(), this.language);
-      }
-      // sort numbers
-      return a - b;
+      // See: https://stackoverflow.com/questions/19993639/difference-in-performance-between-calling-localecompare-on-string-objects-and-c
+      let aName = a.toLowerCase();
+      let bName = b.toLowerCase();
+      // Flawless, but not localized
+      // if (aName < bName)
+      //     return -1;
+      //   if (aName == bName)
+      //     return 0;
+      //   else
+      //     return 1;
+
+      // Perf issues...
+      // let flag = aName - bName;
+      // return _.isNaN(flag) ? aName.localeCompare(bName, this.language) : flag;
+
+      // Perf issues but a bit faster than localeCompare...
+      let collator = new Intl.Collator(this.language);
+      let flag = aName - bName;
+      return _.isNaN(flag) ? collator.compare(aName, bName) : flag;
     }
   }
 };
