570f9cd8070aa018fd720d9a6d89429fdc54c5d7
Keven Rousseau <keven.rousseau@canada.ca>
2018-07-10 14:57:44 -0400

    Missing dot at end of sentence.

--- resources/lang/fr/errors.php
+++ resources/lang/fr/errors.php
@@ -3,7 +3,7 @@
 return [
     'general'          => 'Execption générale. Contactez votre administrateur.',
     'not_found'        => 'Non trouvé',
-    'forbidden'        => 'Autorisations insuffisantes',
+    'forbidden'        => 'Autorisations insuffisantes.',
     'bad_request'      => 'Mauvaise requête. Rafraichissez votre page.',
     'server_error'     => 'Erreur serveur. Rafraichissez votre page.',
     'error_edit_admin' => 'Impossible d\'éditer le compte administrateur.',
