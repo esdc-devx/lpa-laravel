c35649fea326ff55f16a43b7db55d95e2559f31b
Keven Rousseau <keven.rousseau@canada.ca>
2018-07-10 08:00:11 -0400

    Bad merge

--- resources/assets/js/views/project/project-create.vue
+++ resources/assets/js/views/project/project-create.vue
@@ -15,7 +15,7 @@
             v-validate="'required'"
             auto-complete="off">
           </el-input>
-          <form-error v-for="error in verrors.collect('name')" :key="error.id">{{ error }}</form-error>
+          <form-error name="name"></form-error>
         </el-form-item>
         <el-form-item :label="$tc('entities.general.organizational_units')" for="organizationalUnit" :class="['is-required', {'is-error': verrors.collect('organizationalUnit').length }]" prop="organizational_units">
           <el-select
@@ -34,7 +34,7 @@
               :value="item.id">
             </el-option>
           </el-select>
-          <form-error v-for="error in verrors.collect('organizationalUnit')" :key="error.id">{{ error }}</form-error>
+          <form-error name="organizationalUnit"></form-error>
         </el-form-item>
 
         <el-form-item class="form-footer">
