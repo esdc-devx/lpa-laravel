b345a93204ed103621c234b07ba6248775d69373
Francis Mawn <francis.mawn@canada.ca>
2018-07-13 09:47:19 -0400

    Fix load method on ProcessManager class

--- app/Process/ProcessManager.php
+++ app/Process/ProcessManager.php
@@ -154,11 +154,15 @@ class ProcessManager {
     {
         // Load process instance from id.
         if (is_numeric($processInstance)) {
-            $processInstance = ProcessInstance::withProcessDetails()->findOrFail($processInstance);
+            $processInstance = ProcessInstance::findOrFail($processInstance);
         }
         // Load process instance from engine process instance id.
         elseif (is_string($processInstance)) {
-            $processInstance = ProcessInstance::where('engine_process_instance_id', $processInstance)->withProcessDetails()->firstOrFail();
+            $processInstance = ProcessInstance::where('engine_process_instance_id', $processInstance)->firstOrFail();
+        }
+        // Ensure argument passed is an instance of process instance model.
+        elseif (! $processInstance instanceof ProcessInstance) {
+            throw new \Exception('Could not load process instance. Invalid argument provided.');
         }
 
         $this->processInstance = $processInstance;
