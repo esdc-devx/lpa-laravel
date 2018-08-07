d9e260aa5cba1d1b9e92d0ae06b7b0d1461c3966
Keven Rousseau <keven.rousseau@canada.ca>
2018-08-07 07:36:47 -0400

    Create architecture plan assessment form

---
+++ resources/lang/en/forms/architecture_plan_assessment.php
@@ -0,0 +1,31 @@
+<?php
+
+return [
+    'title' => 'Assessment',
+    'tabs' => [
+        'assessment'      => 'Assessment',
+    ],
+    'is_process_cancelled' => [
+        'label'       => 'Project Cancellation',
+        'option'      => 'I would like to cancel this project.',
+        'description' => 'An indicator of whether or not the project has been cancelled.',
+    ],
+    'process_cancellation_rationale' => [
+        'label'       => 'Cancellation Rationale',
+        'instruction' => 'Please provide an explanation if the project is cancelled.',
+        'description' => 'The rationale for the project cancellation.',
+    ],
+    'process_form_decision_id' => [
+        'label'       => 'Decision',
+        'description' => 'The decision on the Business Case.',
+    ],
+    'assessment_comment' => [
+        'label'       => 'Comments',
+        'instruction' => 'Please provide an explanation if adjustments are required.',
+        'description' => 'Comments supporting the decision on the Business Case.',
+    ],
+    'assessment_date' => [
+        'label'       => 'Assessment Date',
+        'description' => 'The date on which the assessment was made.',
+    ]
+];
