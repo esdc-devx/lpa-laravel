a450100f4d41ba2b99c3308d3109e5559530029a
Francis Mawn <francis.mawn@canada.ca>
2018-08-08 14:36:23 -0400

    Update label when form requires adjustments

--- database/seeds/StatesTableSeeder.php
+++ database/seeds/StatesTableSeeder.php
@@ -12,123 +12,123 @@ class StatesTableSeeder extends Seeder
                 [
                     'name_key' => 'new',
                     'name_en'  => 'New',
-                    'name_fr'  => 'Nouveau'
+                    'name_fr'  => 'Nouveau',
                 ],
                 [
                     'name_key' => 'defined',
                     'name_en'  => 'Defined',
-                    'name_fr'  => 'Défini'
+                    'name_fr'  => 'Défini',
                 ],
                 [
                     'name_key' => 'approved',
                     'name_en'  => 'Approved',
-                    'name_fr'  => 'Approuvé'
+                    'name_fr'  => 'Approuvé',
                 ],
                 [
                     'name_key' => 'cancelled',
                     'name_en'  => 'Cancelled',
-                    'name_fr'  => 'Annulé'
+                    'name_fr'  => 'Annulé',
                 ],
                 [
                     'name_key' => 'active',
                     'name_en'  => 'Active',
-                    'name_fr'  => 'Actif'
+                    'name_fr'  => 'Actif',
                 ],
                 [
                     'name_key' => 'active-full',
                     'name_en'  => 'Active (Full)',
-                    'name_fr'  => 'Actif (Complet)'
+                    'name_fr'  => 'Actif (Complet)',
                 ],
                 [
                     'name_key' => 'completed',
                     'name_en'  => 'Completed',
-                    'name_fr'  => 'Complété'
+                    'name_fr'  => 'Complété',
                 ],
             ],
             'process-instance' => [
                 [
                     'name_key' => 'active',
                     'name_en'  => 'Running',
-                    'name_fr'  => 'En cours'
+                    'name_fr'  => 'En cours',
                 ],
                 [
                     'name_key' => 'suspended',
                     'name_en'  => 'Suspended',
-                    'name_fr'  => 'Suspendu'
+                    'name_fr'  => 'Suspendu',
                 ],
                 [
                     'name_key' => 'completed',
                     'name_en'  => 'Completed',
-                    'name_fr'  => 'Complété'
+                    'name_fr'  => 'Complété',
                 ],
                 [
                     'name_key' => 'externally-terminated',
                     'name_en'  => 'Cancelled',
-                    'name_fr'  => 'Annulé'
+                    'name_fr'  => 'Annulé',
                 ],
                 [
                     'name_key' => 'internally-terminated',
                     'name_en'  => 'Cancelled',
-                    'name_fr'  => 'Annulé'
+                    'name_fr'  => 'Annulé',
                 ],
                 [
                     'name_key' => 'cancelled',
                     'name_en' => 'Cancelled',
-                    'name_fr' => 'Annulé'
+                    'name_fr' => 'Annulé',
                 ],
             ],
             'process-step' => [
                 [
                     'name_key' => 'locked',
                     'name_en'  => 'Locked',
-                    'name_fr'  => 'Verrouillé'
+                    'name_fr'  => 'Verrouillé',
                 ],
                 [
                     'name_key' => 'unlocked',
                     'name_en'  => 'Unlocked',
-                    'name_fr'  => 'Déverrouillé'
+                    'name_fr'  => 'Déverrouillé',
                 ],
                 [
                     'name_key' => 'done',
                     'name_en'  => 'Done',
-                    'name_fr'  => 'Complété'
+                    'name_fr'  => 'Complété',
                 ],
                 [
                     'name_key' => 'cancelled',
                     'name_en'  => 'Cancelled',
-                    'name_fr'  => 'Annulé'
+                    'name_fr'  => 'Annulé',
                 ]
             ],
             'process-form' => [
                 [
                     'name_key' => 'locked',
                     'name_en'  => 'Locked',
-                    'name_fr'  => 'Verrouillé'
+                    'name_fr'  => 'Verrouillé',
                 ],
                 [
                     'name_key' => 'unlocked',
                     'name_en'  => 'Unlocked',
-                    'name_fr'  => 'Déverrouillé'
+                    'name_fr'  => 'Déverrouillé',
                 ],
                 [
                     'name_key' => 'submitted',
                     'name_en'  => 'Submitted',
-                    'name_fr'  => 'Soumis'
+                    'name_fr'  => 'Soumis',
                 ],
                 [
                     'name_key' => 'rejected',
-                    'name_en'  => 'Rejected',
-                    'name_fr'  => 'Rejeté'
+                    'name_en' => 'Requires Adjustments',
+                    'name_fr' => 'Nécessite des ajustements',
                 ],
                 [
                     'name_key' => 'done',
                     'name_en'  => 'Done',
-                    'name_fr'  => 'Complété'
+                    'name_fr'  => 'Complété',
                 ],
                 [
                     'name_key' => 'cancelled',
                     'name_en'  => 'Cancelled',
-                    'name_fr'  => 'Annulé'
+                    'name_fr'  => 'Annulé',
                 ]
             ],
         ];
