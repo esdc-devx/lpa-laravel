ac4a4607e2518fbce1a55a868f7dfe1a3a4fc111
Keven Rousseau <keven.rousseau@canada.ca>
2018-08-13 14:15:43 -0400

    Fix performance issues of input number

--- resources/assets/js/config.js
+++ resources/assets/js/config.js
@@ -2,5 +2,6 @@ export default Object.freeze({
   DEBUG: process.env.NODE_ENV !== 'production',
   DEFAULT_LANG: document.querySelector('html').lang,
   // this is to ensure that actions are throttled to avoid user click spam
-  THROTTLE_WAIT_TIME: 1500
+  THROTTLE_WAIT_TIME: 1500,
+  DEBOUNCE_WAIT_TIME: 500
 });
