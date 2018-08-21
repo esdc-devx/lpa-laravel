f15c03bbfd587b47ae6c9fdd3f00f44c8b07aae6
Patrick Messier <Patrick.Messier@canada.ca>
2018-08-21 12:11:23 -0400

    Revamp Home page as per specifications. Refactor  translateY invocation.

--- resources/assets/js/views/home.vue
+++ resources/assets/js/views/home.vue
@@ -1,20 +1,42 @@
 <template>
   <div class="home content">
-    <el-row type="flex" justify="center">
-      <el-col :span="24"><h2>{{ trans('pages.home.welcome', { name: user.first_name }) }}</h2></el-col>
+    <el-row type="flex" justify="center" :gutter="18">
+      <el-col :span="18">
+        <h2 v-if="isAdminOrOwner" v-html="trans('pages.home.title_owner')"></h2>
+        <h2 v-else v-html="trans('pages.home.title_non_owner')"></h2>
+      </el-col>
     </el-row>
-    <el-row type="flex" justify="center">
-      <el-col :span="24">
-        <p v-html="trans('pages.home.instruction', { href: `${language}/getting-started` })"></p>
+    <el-row type="flex" justify="center" :gutter="18">
+      <el-col class="owl-wrap" :span="18">
+        <i class="el-icon-lpa-owl"></i>
+      </el-col>
+    </el-row>
+    <el-row type="flex" justify="center" :gutter="14">
+      <el-col :span="14">
+        <p v-if="isAdminOrOwner" v-html="trans('pages.home.instruction_owner')"></p>
+        <p v-else v-html="trans('pages.home.instruction_non_owner')"></p>
+        <p v-html="trans('pages.home.instruction_everyone')"></p>
       </el-col>
     </el-row>
     <!-- Actions -->
-    <el-row type="flex" class="row-bg actions" justify="center" :gutter="24">
-      <el-col :span="3">
-        <el-button @click="$router.push(`${language}/projects/create`)">
-          <i class="el-icon-edit-outline"></i>
-          <p>{{ trans('pages.project_list.create_project') }}</p>
+    <el-row v-if="isAdminOrOwner" type="flex" class="actions" justify="center" :gutter="15" align="top">
+      <el-col :span="7" >
+        <el-button class="action" @click="$router.push(`${language}/projects/create`)" icon="el-icon-lpa-projects">
+          <span class="action-instruction" v-html="trans('pages.project_list.create_project')"></span>
+          <span class="action-description" v-html="trans('pages.home.description_project')"></span>
         </el-button>
+      </el-col>
+      <!-- @todo: Uncomment the following section when the Create Learning Product page is implemented. See user story lpa-4957.  -->
+      <!-- <el-col :span="7" :offset="1">
+        <el-button class="action" @click="$router.push(`${language}/products/create`)" icon="el-icon-lpa-learning-product">
+          <span class="action-instruction" v-html="trans('pages.product_list.create_product')"></span>
+          <span class="action-description" v-html="trans('pages.home.description_learning_product')"></span>
+        </el-button>
+      </el-col> -->
+    </el-row>
+    <el-row v-if="isAdminOrOwner" type="flex" justify="center" :gutter="12">
+      <el-col :span="12">
+        <p v-html="trans('pages.home.tip_owner')"></p>
       </el-col>
     </el-row>
   </div>
@@ -30,8 +52,12 @@
     computed: {
       ...mapGetters({
         language: 'language',
-        user: 'users/current'
-      })
+        user: 'users/current',
+        hasRole: 'users/hasRole',
+      }),
+      isAdminOrOwner() {
+        return this.hasRole('admin') || this.hasRole('owner');
+      }
     },
 
     mounted() {
@@ -42,6 +68,7 @@
 
 <style lang="scss">
   @import '~@sass/abstracts/vars';
+  @import '~@sass/abstracts/mixins/helpers';
 
   .home {
     // since there is no breadcrumbs bar on the home page,
@@ -58,21 +85,63 @@
 
     h2 {
       font-size: 3rem;
+      margin-bottom: 0;
+    }
+
+    .owl-wrap {
+      position: relative;
+      text-align: center;
+      &:before, &:after {
+        content: '';
+        width: calc(50% - #{$svg-icons-size-large / 2});
+        height: 1px;
+        background-color: $--color-primary;
+        @include vertical-center(absolute);
+      }
+      &:before {
+        left: 0;
+      }
+      &:after {
+        right: 0;
+      }
+      i.el-icon-lpa-owl {
+        @include size($svg-icons-size-large);
+      }
     }
 
     .actions {
-      button {
-        white-space: initial;
+      align-items: stretch;
+      .action {
+        text-align: center;
+        white-space: normal;
         width: 100%;
         height: 100%;
-        p {
-          margin-bottom: 0;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        border-color: transparent;
+        &:hover {
+          border-color: $--button-default-border;
         }
-      }
-      i {
-        font-size: 3rem;
-        text-align: center;
-        display: block;
+        i {
+          @include size($svg-icons-size-medium);
+          &.el-icon-lpa-projects {
+            @include svg(projects, $--color-primary);
+          }
+          &.el-icon-lpa-learning-product {
+            @include svg(learning-product, $--color-primary);
+          }
+        }
+        span {
+          display: block;
+          margin-top: 1em;
+          line-height: 1.5em;
+          width: 100%;
+          &.action-instruction {
+            text-decoration: underline;
+            font-weight: bold;
+          }
+        }
       }
     }
   }
---
+++ resources/lang/fr/pages/product_list.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'create_product' => 'Créer un produit d\'apprentissage',
+];
