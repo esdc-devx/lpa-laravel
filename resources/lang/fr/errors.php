b32370078cff286192d66b2520a4e21c2edf5fee
Francis Mawn <francis.mawn@canada.ca>
2018-08-22 16:17:19 -0400

    Refactor policies and exceptions handling

--- resources/lang/fr/errors.php
+++ resources/lang/fr/errors.php
@@ -1,12 +1,13 @@
 <?php
 
 return [
-    'general'          => 'Execption générale. Contactez votre administrateur.',
-    'not_found'        => 'Non trouvé',
-    'forbidden'        => 'Autorisations insuffisantes.',
-    'timeout'          => 'La connection avec le serveur a expiré.',
-    'bad_request'      => 'Mauvaise requête. Rafraichissez votre page.',
-    'server_error'     => 'Erreur serveur. Rafraichissez votre page.',
-    'error_edit_admin' => 'Impossible d\'éditer le compte administrateur.',
-    'csrf_not_found'   => 'Token CSRF introuvable: https://laravel.com/docs/csrf#csrf-x-csrf-token',
+    'general'                 => 'Execption générale. Contactez votre administrateur.',
+    'forbidden'               => 'Autorisations insuffisantes.',
+    'timeout'                 => 'La connection avec le serveur a expiré.',
+    'bad_request'             => 'Mauvaise requête. Rafraichissez votre page.',
+    'server_error'            => 'Erreur serveur. Rafraichissez votre page.',
+    'error_edit_admin'        => 'Impossible d\'éditer le compte administrateur.',
+    'csrf_not_found'          => 'Token CSRF introuvable: https://laravel.com/docs/csrf#csrf-x-csrf-token',
+    'insufficient_privileges' => 'Vous ne disposez plus des autorisations nécessaires pour effectuer cette opération. Communiquez avec votre administrateur de l\'APA pour plus de détails.',
+    'operation_denied'        => 'L\'opération demandée n\'est plus possible.',
 ];
