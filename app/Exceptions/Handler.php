1120cfb078b68610d09d702b2b7271265a140d46
Francis Mawn <francis.mawn@canada.ca>
2018-08-24 17:12:05 -0400

    Add clarification in exception handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -59,6 +59,7 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        // Store exception to return more details in "respondWithError" method.
         $this->exception = $exception;
 
         // Render Rest API errors.
