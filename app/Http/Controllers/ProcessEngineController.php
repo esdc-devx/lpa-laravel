332182ef42381eaa8fe8ef04ca06ebce49c21239
Francis Mawn <francis.mawn@canada.ca>
2018-11-06 09:31:52 -0500

    Reformat notification code

--- app/Http/Controllers/ProcessEngineController.php
+++ app/Http/Controllers/ProcessEngineController.php
@@ -11,12 +11,15 @@ class ProcessEngineController extends APIController
     public function notifications(Request $request, $emailKey, $engineProcessInstanceId)
     {
         //@todo: Implement email notification logic.
+        $response = 'Message [:email_key] for process instance id [:engine_process_instance_id] has been successfully sent to [:addressee_list] on [:date].';
+        $replace = [
+            ':email_key'                  => $emailKey,
+            ':engine_process_instance_id' => $engineProcessInstanceId,
+            ':addressee_list'             => join($request->addresseeList, ', '),
+            ':date'                       => Carbon::now()->format('Y-m-d H:i:s'),
+        ];
         return $this->respond(
-            str_replace(
-                [':email_key', ':engine_process_instance_id', ':addressee_list', ':date'],
-                [$emailKey, $engineProcessInstanceId, join($request->addresseeList, ', '), Carbon::now()],
-                'Message [:email_key] for process instance id [:engine_process_instance_id] has been successfully sent to [:addressee_list] on [:date].'
-            )
+            str_replace(array_keys($replace), array_values($replace), $response)
         );
     }
 }
