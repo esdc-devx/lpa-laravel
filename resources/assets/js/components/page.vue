ca35f82bf7a552d5e1da3b612520981d50facdbd
Keven Rousseau <keven.rousseau@canada.ca>
2018-11-19 15:00:55 -0500

    Remove dead code

--- resources/assets/js/components/page.vue
+++ resources/assets/js/components/page.vue
@@ -100,7 +100,6 @@
 
       // Utils
       scrollToTop() {
-        this.$parent.$el.scrollTop = 0;
         document.querySelectorAll('.el-main')[0].scrollTop = 0;
         // IE11 scroll to top
         document.querySelectorAll('.content-wrap')[0].scrollTop = 0
